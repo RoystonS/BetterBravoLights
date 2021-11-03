1b9e9205fca370e483bb5f5d2f253db3a8573003
Royston Shufflebotham <royston@shufflebotham.org>
2021-11-03 22:21:41 +0000

    Specify that assemble-wasm-for-ide is copying to a directory

--- assemble-wasm-for-ide.cmd
+++ assemble-wasm-for-ide.cmd
@@ -3,4 +3,4 @@ rd/s/q BravoLights\bin\Release\net5-windows\better-bravo-lights-lvar-module
 
 rem Builds the WASM module and installs it into the Debug output directory for the project.
 "%MSFS_SDK%Tools\bin\fspackagetool.exe" MSFSWASMProject\BetterBravoLightsLVars.xml -outputdir BravoLights\bin\Debug\net5.0-windows
-xcopy /c/d/e/h/k BravoLights\bin\Debug\net5.0-windows\better-bravo-lights-lvar-module BravoLights\bin\Release\net5.0-windows\better-bravo-lights-lvar-module
+echo D | xcopy /c/d/e/h/k BravoLights\bin\Debug\net5.0-windows\better-bravo-lights-lvar-module BravoLights\bin\Release\net5.0-windows\better-bravo-lights-lvar-module
