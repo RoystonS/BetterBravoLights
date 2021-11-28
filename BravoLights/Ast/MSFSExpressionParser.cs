64af67760d3dc98150ca9d6a5f68d056b6191a12
Royston Shufflebotham <royston@shufflebotham.org>
2021-11-28 23:52:26 +0000

    Improve error messages for missing A: var units

--- BravoLights/Ast/MSFSExpressionParser.cs
+++ BravoLights/Ast/MSFSExpressionParser.cs
@@ -2,6 +2,7 @@
 using BravoLights.Common.Ast;
 using sly.lexer;
 using sly.parser.generator;
+using System;
 
 namespace BravoLights.Ast
 {
@@ -24,9 +25,21 @@ namespace BravoLights.Ast
         [Production("numeric_literal: SIMVAR")]
         public IAstNode SimVarExpression(Token<ExpressionToken> simvarToken)
         {
+            // text will be
+            // A:SIMVAR
+            // or
+            // A:SIMVAR, units
+            // (The former isn't valid but we allow it up to this point so that we can give a nice error message about the missing units)
+
             var text = simvarToken.Value[2..];
-            var bits = text.Split(",");
-            var varName = bits[0];
+            var bits = text.Split(",", 2);
+            var varName = bits[0].Trim();
+
+            if (bits.Length == 1)
+            {
+                throw new Exception($"Missing units for variable 'A:{varName}'.");
+            }
+
             var type = bits[1].Trim();
             return new SimVarExpression(varName, type);
         }
