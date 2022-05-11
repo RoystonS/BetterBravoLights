f6bc36b65ebb89525cc57ba34c85b070105c815d
Royston Shufflebotham <royston@shufflebotham.org>
2022-05-11 22:26:21 +0100

    Copy lvar module for release and unit tests

--- assemble-wasm-for-ide.cmd
+++ assemble-wasm-for-ide.cmd
@@ -3,4 +3,9 @@ rd/s/q BravoLights\bin\Release\net5-windows\better-bravo-lights-lvar-module
 
 rem Builds the WASM module and installs it into the Debug output directory for the project.
 "%MSFS_SDK%Tools\bin\fspackagetool.exe" MSFSWASMProject\BetterBravoLightsLVars.xml -outputdir BravoLights\bin\Debug\net5.0-windows
-echo D | xcopy /c/d/e/h/k BravoLights\bin\Debug\net5.0-windows\better-bravo-lights-lvar-module BravoLights\bin\Release\net5.0-windows\better-bravo-lights-lvar-module
+
+rem And another copy for the Release dir
+echo D | xcopy /c/d/e/h/k BravoLights\bin\Debug\net5.0-windows\Packages BravoLights\bin\Release\net5.0-windows\Packages
+
+rem And another copy for unit tests
+echo D | xcopy /c/d/e/h/k BravoLights\bin\Debug\net5.0-windows\Packages BravoLights.Tests\bin\Debug\net5.0-windows\Packages
